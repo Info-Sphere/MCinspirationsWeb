f87bb386771c4db8808470a1fec188e61b0a86fb
Info-Sphere <165199424+Info-Sphere@users.noreply.github.com>
2025-02-09 22:05:18 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,32 +1,32 @@
 window.products = {
     jewelry: [
-        { name: "Silver Necklace", description: "Elegant silver necklace.", price: 49.99, imageUrl: "img/pulseras/pulsera1.jpg", featured: "yes" },
-        { name: "Gold Bracelet", description: "Stylish gold bracelet.", price: 79.99, imageUrl: "img/pulseras/pulsera2.jpg", featured: "no" },
-        { name: "Diamond Earrings", description: "Sparkling diamond earrings.", price: 129.99, imageUrl: "img/pulseras/pulsera3.jpg", featured: "yes" },
-        { name: "Pearl Ring", description: "Classic pearl ring.", price: 59.99, imageUrl: "img/pulseras/pulsera4.jpg", featured: "no" },
-        { name: "Ruby Pendant", description: "Beautiful ruby pendant.", price: 99.99, imageUrl: "img/pulseras/pulsera5.jpg", featured: "yes" },
-        { name: "Sapphire Brooch", description: "Stunning sapphire brooch.", price: 149.99, imageUrl: "img/pulseras/pulsera6.jpg", featured: "no" },
-        { name: "Emerald Anklet", description: "Chic emerald anklet.", price: 69.99, imageUrl: "img/pulseras/pulsera7.jpg", featured: "yes" },
-        { name: "Amethyst Tiara", description: "Regal amethyst tiara.", price: 199.99, imageUrl: "img/pulseras/pulsera8.jpg", featured: "no" }
+        { name: "Silver Necklace", description: "Elegant silver necklace.", price: 49.99, imageUrl: "pulsera1.jpg", featured: "yes" },
+        { name: "Gold Bracelet", description: "Stylish gold bracelet.", price: 79.99, imageUrl: "pulsera2.jpg", featured: "no" },
+        { name: "Diamond Earrings", description: "Sparkling diamond earrings.", price: 129.99, imageUrl: "pulsera3.jpg", featured: "yes" },
+        { name: "Pearl Ring", description: "Classic pearl ring.", price: 59.99, imageUrl: "pulsera4.jpg", featured: "no" },
+        { name: "Ruby Pendant", description: "Beautiful ruby pendant.", price: 99.99, imageUrl: "pulsera5.jpg", featured: "yes" },
+        { name: "Sapphire Brooch", description: "Stunning sapphire brooch.", price: 149.99, imageUrl: "pulsera6.jpg", featured: "no" },
+        { name: "Emerald Anklet", description: "Chic emerald anklet.", price: 69.99, imageUrl: "pulsera7.jpg", featured: "yes" },
+        { name: "Amethyst Tiara", description: "Regal amethyst tiara.", price: 199.99, imageUrl: "pulsera8.jpg", featured: "no" }
     ],
     accessories: [
-        { name: "Accessory 1", description: "Description of Accessory 1", price: 25.00, imageUrl: "img/accesorios/1.jpg", featured: "yes" },
-        { name: "Accessory 2", description: "Description of Accessory 2", price: 30.00, imageUrl: "img/accesorios/a2.jpg", featured: "no" },
-        { name: "Accessory 3", description: "Description of Accessory 3", price: 15.00, imageUrl: "img/accesorios/a3.jpg", featured: "yes" },
-        { name: "Accessory 4", description: "Description of Accessory 4", price: 40.00, imageUrl: "img/accesorios/a4.jpg", featured: "no" },
-        { name: "Accessory 5", description: "Description of Accessory 5", price: 20.00, imageUrl: "img/accesorios/a5.jpg", featured: "yes" },
-        { name: "Accessory 6", description: "Description of Accessory 6", price: 35.00, imageUrl: "img/accesorios/a6.jpg", featured: "no" },
-        { name: "Accessory 7", description: "Description of Accessory 7", price: 10.00, imageUrl: "img/accesorios/a7.jpg", featured: "yes" },
-        { name: "Accessory 8", description: "Description of Accessory 8", price: 45.00, imageUrl: "img/accesorios/a8.jpg", featured: "no" }
+        { name: "Accessory 1", description: "Description of Accessory 1", price: 25.00, imageUrl: "a1.jpg", featured: "yes" },
+        { name: "Accessory 2", description: "Description of Accessory 2", price: 30.00, imageUrl: "a2.jpg", featured: "no" },
+        { name: "Accessory 3", description: "Description of Accessory 3", price: 15.00, imageUrl: "a3.jpg", featured: "yes" },
+        { name: "Accessory 4", description: "Description of Accessory 4", price: 40.00, imageUrl: "a4.jpg", featured: "no" },
+        { name: "Accessory 5", description: "Description of Accessory 5", price: 20.00, imageUrl: "a5.jpg", featured: "yes" },
+        { name: "Accessory 6", description: "Description of Accessory 6", price: 35.00, imageUrl: "a6.jpg", featured: "no" },
+        { name: "Accessory 7", description: "Description of Accessory 7", price: 10.00, imageUrl: "a7.jpg", featured: "yes" },
+        { name: "Accessory 8", description: "Description of Accessory 8", price: 45.00, imageUrl: "a8.jpg", featured: "no" }
     ],
     gifts: [
-        { name: "Gift 1", description: "Description of Gift 1", price: 50.00, imageUrl: "img/regalos/r1.jpg", featured: "yes" },
-        { name: "Gift 2", description: "Description of Gift 2", price: 75.00, imageUrl: "img/regalos/r2.jpg", featured: "no" },
-        { name: "Gift 3", description: "Description of Gift 3", price: 30.00, imageUrl: "img/regalos/r3.jpg", featured: "yes" },
-        { name: "Gift 4", description: "Description of Gift 4", price: 60.00, imageUrl: "img/regalos/r4.jpg", featured: "no" },
-        { name: "Gift 5", description: "Description of Gift 5", price: 25.00, imageUrl: "img/regalos/r5.jpg", featured: "yes" },
-        { name: "Gift 6", description: "Description of Gift 6", price: 80.00, imageUrl: "img/regalos/r6.jpg", featured: "no" },
-        { name: "Gift 7", description: "Description of Gift 7", price: 40.00, imageUrl: "img/regalos/r7.jpg", featured: "yes" },
-        { name: "Gift 8", description: "Description of Gift 8", price: 90.00, imageUrl: "img/regalos/r8.jpg", featured: "no" }
+        { name: "Gift 1", description: "Description of Gift 1", price: 50.00, imageUrl: "r1.jpg", featured: "yes" },
+        { name: "Gift 2", description: "Description of Gift 2", price: 75.00, imageUrl: "r2.jpg", featured: "no" },
+        { name: "Gift 3", description: "Description of Gift 3", price: 30.00, imageUrl: "r3.jpg", featured: "yes" },
+        { name: "Gift 4", description: "Description of Gift 4", price: 60.00, imageUrl: "r4.jpg", featured: "no" },
+        { name: "Gift 5", description: "Description of Gift 5", price: 25.00, imageUrl: "r5.jpg", featured: "yes" },
+        { name: "Gift 6", description: "Description of Gift 6", price: 80.00, imageUrl: "r6.jpg", featured: "no" },
+        { name: "Gift 7", description: "Description of Gift 7", price: 40.00, imageUrl: "r7.jpg", featured: "yes" },
+        { name: "Gift 8", description: "Description of Gift 8", price: 90.00, imageUrl: "r8.jpg", featured: "no" }
     ]
 };
